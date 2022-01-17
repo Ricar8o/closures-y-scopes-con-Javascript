d4a57ff59fde4cb499c6fd0af0dd76e19786a5b9
Ricardo <ricardo.8md@gmail.com>
2022-01-17 13:53:37 -0500

    More about closures

--- src/closure/closure.js
+++ src/closure/closure.js
@@ -23,34 +23,3 @@ let myMoneyBox = moneyBox();
 myMoneyBox(4);
 myMoneyBox(6);
 myMoneyBox(8);
-
-
-const miContador = (function() {
-    let _contador = 0;
-
-    function incrementar() {
-        return _contador++;
-    }
-
-    function decrementar() {
-        return _contador--;
-    }
-
-    function valor() {
-        return _contador;
-    }
-
-    return {
-        incrementar,
-        decrementar,
-        valor
-    }
-})();
-
-console.log(miContador.valor());
-console.log(miContador.incrementar());
-miContador.incrementar();
-miContador.incrementar();
-console.log(miContador.valor());
-console.log(miContador.decrementar());
-console.log(miContador.valor());
